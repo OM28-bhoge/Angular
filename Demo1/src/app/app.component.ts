9bae8c7e99d09186c2b93ee3af78bb060fdd911c
Om <ombhoge1234@gmail.com>
2023-02-21 10:25:33 +0530

    Commit

---
+++ Demo1/src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'MyfirstAngularApp';
+  a1="TestCode";
+
+  getData() 
+  {
+    return "Value from Method";
+  }
+
+  val = "Value from this keyword";
+  getData1()
+  {
+    return this.val;
+  }
+
+  obj={name:"Om",age:"22"};
+
+  a=500;
+  b=200;
+
+  currentURL=window.location.href;
+
+  GetYTChannel()
+  {
+    alert("My Alert");
+  }
+
+  str="Get String Value";
+  GetValue()
+  {
+    alert(this.str);
+  }
+
+  GetValuebyParam(name:any)
+  {
+    alert(name);
+  }
+
+  strName = '' ;
+  GetValue1(val:any)
+  {
+    console.warn(val);
+    this.strName = val ;
+  }
+
+  number1 :any = 0;
+  number2 :any = 0;
+  sum = 0;
+  
+  calc(num1:any,num2:any)
+  {
+    this.sum = parseFloat(num1) + parseFloat(num2);
+  }
+  
+  EventHandling(val:any)
+  {
+    alert(val);
+    console.warn(val)
+  }
+}
